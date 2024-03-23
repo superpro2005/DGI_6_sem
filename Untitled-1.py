93442fb1f8776d0c9e0f4d434bf1fe7601c7c7dd
andrew <minimum32@list.ru>
2024-03-23 12:59:56 +0300

    1

--- Untitled-1.py
+++ Untitled-1.py
@@ -3,8 +3,7 @@ def my_func():
     my_var = 'some text'
     print(my_var)
 
-print(my_var) 
-
+# print(my_var) 
 my_func()
 
-print(my_var)
+# print(my_var) нельзя выводить пеерменную заданную в функции
