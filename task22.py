890cd33a4654f120906274835fb44c3a1bf064c5
andrew <minimum32@list.ru>
2024-03-23 13:03:16 +0300

    2

---
+++ task22.py
@@ -0,0 +1,10 @@
+my_var1 = 5
+my_var2 = 10
+
+def my_func():
+    my_var1=15
+    print(my_var1)   
+    print(my_var2)
+    
+my_func()
+# ошибка не возни кадла из за того что переменная была определена ране
