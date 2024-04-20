ad555dc847d58f4780701904db61695e897fb593
andrey <minimum32@list.ru>
2024-04-20 12:40:34 +0300

    123

--- seminar3/task1.py
+++ seminar3/task1.py
@@ -1,36 +1,38 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import copy
+from math import ceil 
 
-file0bj = open("1kHz_44100Hz_16bit_05sec.wav", mode = "rb")
+class audio():
+    def __init__(self,filename:str) -> None:
+        file0bj = open("1kHz_44100Hz_16bit_05sec.wav", mode = "rb")
+        data = file0bj.read()
+        file_size_inbytes = data [4:8] 
+        file_size = int.from_bytes(file_size_inbytes, byteorder = "little")
 
-data = file0bj.read()
+        print ('рамер файла - ',file_size)
 
-file_size_inbytes = data [4:8] 
-file_size = int.from_bytes(file_size_inbytes, byteorder = "little")
+        num_of_channels_in_byte = data[22:24]
+        num_of_channels = int.from_bytes(num_of_channels_in_byte, byteorder = "little")
 
-print ('рамер файла - ',file_size)
+        print ('colvo canalov - ',num_of_channels)
 
-num_of_channels_in_byte = data[22:24]
-num_of_channels = int.from_bytes(num_of_channels_in_byte, byteorder = "little")
+        audio_data_size_in_bytes = data[40:44]
+        audio_data_size =int.from_bytes(audio_data_size_in_bytes, byteorder = "little")
 
-print ('colvo canalov - ',num_of_channels)
+        print ('audio_data -',audio_data_size)
 
-audio_data_size_in_bytes = data[40:44]
-audio_data_size =int.from_bytes(audio_data_size_in_bytes, byteorder = "little")
+        audio_amps = []
 
-print ('audio_data -',audio_data_size)
+        sample_rate_in_bytes = data [24:28]
+        sample_rate = int.from_bytes(sample_rate_in_bytes, byteorder = "little")
 
-audio_amps = []
+        print ('chastota discr - ',sample_rate)
 
-sample_rate_in_bytes = data [24:28]
-sample_rate = int.from_bytes(sample_rate_in_bytes, byteorder = "little")
-
-print ('chastota discr - ',sample_rate)
-
-for i in range (0,audio_data_size,2):
-    amp_in_byte = data[44+i:44+i+2]
-    amp = int.from_bytes(amp_in_byte, byteorder = "little", signed = True)
-    audio_amps.append(amp)
+        for i in range (0,audio_data_size,2):
+            amp_in_byte = data[44+i:44+i+2]
+            amp = int.from_bytes(amp_in_byte, byteorder = "little", signed = True)
+            audio_amps.append(amp)
 
 # xdata = range(0,len(audio_amps)/sample_rate, 1/sample_rate)
 xdata = np.linspace(0,len(audio_amps)/sample_rate, len(audio_amps))
