ef2b7d7f41fdf62b9ae33ad302149a359ed82b78
superpro2005 <minimum32@list.ru>
2024-06-02 19:23:37 +0300

    qweqweqwe

--- seminar 5/client.py
+++ seminar 5/client.py
@@ -1,7 +1,32 @@
 import socket
+from _thread import start_new_thread
 
 client = socket.socket()
 
-client.connect(('localhost', int(3000)))
+client.connect(('192.168.51.238',3000))
 
+data = client.recv(1024)
+print('Сообщение в бинарном виде', data)
+print('входящие сообщения ', data.decode())
+
+message = 'Привет,сервер!'
+data = message.encode()
+client.send(data)
+
+def getMessages():
+    while True:
+        data = client.recv(1024)
+        print('', data.decode())
+        if data.decode()=='Закрывай!' :
+            break
+    client.close()
+
+start_new_thread(getMessages,())
+
+while True:
+    message = input('Введите сообщение ')
+    data = message.encode()
+    client.send(data)
+    if data.decode()=='Закрывай!' :
+        break
 client.close()
